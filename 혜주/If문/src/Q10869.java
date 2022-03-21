3633455856f6ba69cfee047d5542a6efa34548a3
권혜주 <2k3m54@gmail.com>
2022-03-21 21:35:54 +0900

    [2022-03-21 권혜주] If문: ~ Q.18108

---
+++ 혜주/If문/src/Q10869.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Q10869 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A + B);
+		System.out.println(A - B);
+		System.out.println(A * B);
+		System.out.println(A / B);
+		System.out.println(A % B);
+	}
+
+}
--- 혜주/If문/src/Type001.java
+++
@@ -1,9 +0,0 @@
-
-public class Type001 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
