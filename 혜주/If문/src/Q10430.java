5ca25e8d7b09ccaf14755e6dec727d606e62cbf4
leechun1095 <94797128+leechun1095@users.noreply.github.com>
2022-03-22 22:11:01 +0900

    Update Q10430.java

--- 혜주/If문/src/Q10430.java
+++ 혜주/If문/src/Q10430.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 public class Q10430 {
 
 	public static void main(String[] args) {
-		
+		/*
 		Scanner sc = new Scanner(System.in);
 		int A = sc.nextInt();
 		int B = sc.nextInt();
 		int C = sc.nextInt();
+		*/
+		
+		// BufferedReader 사용해 보세요. Scanner 보다 훨씬 성능적으로 좋다고 합니다. 앞으로 계속 써야해서 쓰다보면 외워짐!
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine().split(" "));
+		int B = Integer.parseInt(br.readLine().split(" "));
+		int C = Integer.parseInt(br.readLine().split(" "));
 		
 		System.out.println((A+B)%C);
 		System.out.println(((A%C) + (B%C))%C);
