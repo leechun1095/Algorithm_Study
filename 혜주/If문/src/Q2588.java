f854382f72bf3600d9d18d082ac692700a0a4376
권혜주 <2k3m54@gmail.com>
2022-03-22 20:23:57 +0900

    [2022-03-22 권혜주]

---
+++ 혜주/If문/src/Q2588.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q2588 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		String bStr = Integer.toString(B);
+		
+		System.out.println(A * Integer.parseInt(bStr.substring(2)));
+		System.out.println(A * Integer.parseInt(bStr.substring(1,2)));
+		System.out.println(A * Integer.parseInt(bStr.substring(0,1)));
+		System.out.println(A * B);
+		
+	}
+
+}
