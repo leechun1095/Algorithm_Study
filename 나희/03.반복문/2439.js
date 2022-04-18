444d599ccb7745022d0720dcf05d50775f54d8a6
skgml0 <hnh2714@gmail.com>
2022-04-18 23:46:29 +0900

    [2022.04.18]

--- 나희/03.반복문/2439.js
+++ 나희/03.반복문/2439.js
@@ -1,10 +1,11 @@
 const n = require("fs").readFileSync("/dev/stdin").toString();
 // const n = 5;
-let blank = " ";
-let str = "*";
-let answer = [];
+let answer = Array.from({ length: n }, (v) => " ");
+// let answer = [] * n;
+// console.log(answer);
 for (i = +n - 1; i >= 0; i--) {
-  answer.push(`${blank.repeat(i)} ${str.repeat(+n - i)}`);
+  answer.splice(i, 1, "*");
+  console.log(answer.join(""));
 }
 // 마지막 공백제거
-console.log(answer.join("\n"));
+// console.log(answer.join("\n"));
