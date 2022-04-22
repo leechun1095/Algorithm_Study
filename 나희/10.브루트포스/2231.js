93ffd240bfe075514c26f9273264892a0b7bb008
skgml0 <hnh2714@gmail.com>
2022-04-22 22:13:58 +0900

    [2022.04.22]

--- 나희/10.브루트포스/2231.js
+++ 나희/10.브루트포스/2231.js
@@ -1,13 +1,14 @@
-const n = require("fs")
-  .readFileSync("dev/stdin")
-  .toString()
-  .split("")
-  .map(Number);
-// const n = 1;
+const n = +require("fs").readFileSync("/dev/stdin").toString().trim();
 let sum = 0;
 let array = [];
 for (let i = n - 1; i > 0; i--) {
-  sum = i + parseInt(i / 100) + (parseInt(i / 10) % 10) + (i % 10);
+  sum =
+    i +
+    i
+      .toString()
+      .split("")
+      .map((n) => parseInt(n))
+      .reduce((p, c) => p + c, 0);
   // console.log(`i: ${i} sum: ${sum}`);
   if (sum === n) {
     array.push(i);
