1834707e275921f0cf0f0aa4e0d97423325241ad
권혜주 <2k3m54@gmail.com>
2022-04-24 10:00:04 +0900

    [2022.04.24]

---
+++ 혜주/src/K_정렬/Q18870.java
@@ -0,0 +1,50 @@
+package K_정렬;
+//만약 정확한 값이 필요 없고 값의 대소 관계만 필요하다면, 모든 수를 0 이상 N 미만의 수로 바꿀 수 있습니다.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+public class Q18870 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] origin = new int[N];
+		int[] sorted = new int[N];
+		HashMap<Integer, Integer> rankingMap = new HashMap<Integer, Integer>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<N; i++) {
+			sorted[i] = origin[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(sorted);
+		
+		int rank = 0;
+		for(int v: sorted) {
+			if(!rankingMap.containsKey(v)) {
+				rankingMap.put(v, rank);
+				rank++;
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int key: origin) {
+			int ranking = rankingMap.get(key);
+			sb.append(ranking).append(' ');
+		}
+		
+		System.out.println(sb);
+		
+		br.close();
+	}
+}
