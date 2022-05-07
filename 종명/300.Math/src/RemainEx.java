31c7cb21fbbb05ac0ed5adb53a55377d956f5cff
Greg <Gregorio3.kim@gmail.com>
2022-05-07 20:54:59 +0900

    1

--- 종명/300.Math/src/RemainEx.java
+++ 종명/300.Math/src/RemainEx.java
@@ -1,9 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class RemainEx {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		
+		String[] arr = input.split(" ");
+		int A = Integer.parseInt(arr[0]);
+		int B = Integer.parseInt(arr[1]);
+		int C = Integer.parseInt(arr[2]);
+		
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C );
+		System.out.println((A*B)%C);
+		System.out.println(((A%C) * (B%C))%C);
 	}
 
 }
