6d2ebdb0fe0bddccd69a40a6b40cec7ff35d76b8
skgml0 <hnh2714@gmail.com>
2022-05-21 10:04:14 +0900

    [2022.05.19]

---
+++ 나희/400-다이나믹 프로그래밍1/1463.js
@@ -0,0 +1,20 @@
+const input = require("fs").readFileSync("/dev/stdin").toString();
+// const input = +`10`.toString();                                                                                                                                         
+const num = Number(input);
+
+const DP = new Array(num + 1).fill(0);
+
+for (let i = 2; i <= num; i++) {
+  DP[i] = DP[i - 1] + 1;
+
+  if (i % 2 === 0) {
+    DP[i] = Math.min(DP[i], DP[i / 2] + 1);
+  }
+
+  if (i % 3 === 0) {
+    DP[i] = Math.min(DP[i], DP[i / 3] + 1);
+  }
+}
+
+console.log(DP[num]);
+// DP[1] = 0 임. 1이 그대로 나와야하기 때문에.
