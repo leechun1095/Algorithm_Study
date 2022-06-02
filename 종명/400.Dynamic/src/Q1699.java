4038a5335e27df196b1356e1211070051b936e21
Greg <Gregorio3.kim@gmail.com>
2022-06-03 02:17:14 +0900

    2022.06.02

---
+++ 종명/400.Dynamic/src/Q1699.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+// Á¦°ö¼öÀÇ ÇÕ
+public class Q1699 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] d = new int[n+1];
+        
+        for (int i=1; i<=n; i++) {
+            d[i] = i;
+            for (int j=1; j*j <= i; j++) {
+                if (d[i] > d[i-j*j] + 1) {
+                    d[i] = d[i-j*j] + 1;
+                }
+            }
+        }
+        
+        System.out.println(d[n]);
+    }
+}
