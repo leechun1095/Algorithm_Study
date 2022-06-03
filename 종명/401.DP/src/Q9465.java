31b58676b4719e2bb51746f4677087d0a553191b
Greg <Gregorio3.kim@gmail.com>
2022-06-04 02:03:48 +0900

    2022.06.02

--- 종명/401.DP/src/Q9465.java
+++ 종명/401.DP/src/Q9465.java
@@ -6,8 +6,10 @@ public class Q9465 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.valueOf(br.readLine());
+        
         while (t-- > 0) {
             int n = Integer.valueOf(br.readLine());
+            // 각각의 값을 a[i][0/1] 에 할당 
             long[][] a = new long[n+1][2];
             {
                 String[] line = br.readLine().split(" ");
@@ -21,10 +23,16 @@ public class Q9465 {
                     a[i][1] = Long.valueOf(line[i-1]);
                 }
             }
+            
+            
             long[][] d = new long[n+1][3];
+            // d[i][j] = 2 x i 에서  
             for (int i=1; i<=n; i++) {
+            	// 스티커 뜯지 않음 
                 d[i][0] = Math.max(d[i-1][0],Math.max(d[i-1][1],d[i-1][2]));
+                // 위 스티커 뜯음
                 d[i][1] = Math.max(d[i-1][0],d[i-1][2])+a[i][0];
+                // 아래 스티커 뜯음 
                 d[i][2] = Math.max(d[i-1][0],d[i-1][1])+a[i][1];
             }
             long ans = 0;
