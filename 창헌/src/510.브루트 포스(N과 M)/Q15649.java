ae7a1b2a5d56f5d47269b4a2b906fa6c77f6c8b2
leechun1095 <leechun1095@gmail.com>
2022-06-24 06:27:11 +0900

    [2022.06.23]

---
+++ 창헌/src/510.브루트 포스(N과 M)/Q15649.java
@@ -0,0 +1,55 @@
+package 브루트포스N과M;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q15649 {
+	
+	static int[] arr; // 뽑힌 수의 배열
+	static boolean[] check; // 수의 사용여부
+	
+	public static void main(String[] args) throws IOException {
+		// N과 M (1)
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		check = new boolean[N+1];
+		
+		pick(N, M, 0);
+	}
+	
+	// 수 뽑기(재귀함수)
+	static void pick(int N, int M, int index) {
+		
+		if(index == M) { // M개 만큼 다 뽑았으면 return
+			printArr();
+			return;
+		}
+		
+		for(int i=1; i<=N; i++) {
+			if(!check[i]) { // 이미 뽑힌 수(i)는 사용하지 않는다.
+				check[i] = true; // 사용중
+				arr[index] = i;
+				
+				pick(N, M, index+1); // 재귀
+				
+				check[i] = false; // 사용해제
+			}
+		}
+		
+	}
+
+	private static void printArr() {
+		for(int num: arr) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+	}
+
+}
---
+++ 창헌/src/510.브루트 포스(N과 M)/Q15650.java
@@ -0,0 +1,56 @@
+package 브루트포스N과M;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q15650 {
+	
+	static int[] arr; // 뽑힌 수의 배열
+	static boolean[] check; // 수의 사용여부
+	
+	public static void main(String[] args) throws IOException {
+		// N과 M (2)
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		check = new boolean[N+1];
+		
+		pick(N, M, 1, 0);
+	}
+	
+	// 수 뽑기(재귀함수)
+	// 오름차순: 다음에 올 수는 현재 수보다 커야한다.
+	static void pick(int N, int M, int start, int index) { // start: 이 수부터 시작해라
+		
+		if(index == M) { // M개 만큼 다 뽑았으면 return
+			printArr();
+			return;
+		}
+		
+		for(int i=start; i<=N; i++) {
+			if(!check[i]) { // 이미 뽑힌 수(i)는 사용하지 않는다.
+//				check[i] = true; // 사용중
+				arr[index] = i;
+				
+				pick(N, M, i+1, index+1); // 재귀
+				
+//				check[i] = false; // 사용해제
+			}
+		}
+		
+	}
+
+	private static void printArr() {
+		for(int num: arr) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+	}
+
+}
